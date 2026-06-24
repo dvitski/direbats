5dfecb6ee925b09b02708c0e423510aff563e832
Daniel Orr <orrdaniel73@gmail.com>
2026-06-25 00:12:07 +0100

    feat: publishMods

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,22 +1,8 @@
-import com.matthewprenger.cursegradle.CurseArtifact
-import com.matthewprenger.cursegradle.CurseProject
-import com.matthewprenger.cursegradle.CurseRelation
-import org.kohsuke.github.GHReleaseBuilder
-import org.kohsuke.github.GitHub
-
-buildscript {
-    dependencies {
-        classpath("org.kohsuke", "github-api", "1.+")
-    }
-}
-
 plugins {
     kotlin("jvm").version(System.getProperty("kotlin_version"))
 
     id("fabric-loom")
-
-    id("com.matthewprenger.cursegradle").version("1.+")
-    id("com.modrinth.minotaur").version("2.+")
+    id("me.modmuss50.mod-publish-plugin") version "2.0.1"
 }
 
 base { archivesName.set(extra["archives_base_name"] as String) }
@@ -59,7 +45,7 @@ tasks {
         withSourcesJar()
     }
 
-    jar { from("LICENSE") { rename { "${it}_${base.archivesName.get()}" } } }
+    // jar { from("LICENSE") { rename { "${it}_${base.archivesName.get()}" } } }
     processResources { filesMatching("fabric.mod.json") { expand(mapOf("version" to version)) } }
 }
 
@@ -110,74 +96,33 @@ sourceSets.main {
 
 /* Releasing */
 
-val env: Map<String, String> = System.getenv()!!
-val versionNameText = "[${extra["major_version"]}] ${extra["mod_name"]} ${extra["mod_version"]}"
-val changelogText = File("./gradle", "CHANGELOG.md").readText()
-val supportedVersions = (extra["supported_versions"] as String).split(',')
-val rawReleaseType = extra["release_type"] as String
-val githubRepository = extra["github_repository"] as String
-val githubBranch = extra["github_branch"] as String
-val modrinthId = extra["modrinth_id"] as String
-val curseforgeId = extra["curseforge_id"] as String
+publishMods {
+    file.set(tasks.jar.flatMap { it.archiveFile })
 
-env["GITHUB_TOKEN"]?.run {
-    tasks.register("github") {
-        doLast {
-            val github = GitHub.connectUsingOAuth(this@run)
-            val repository = github.getRepository(githubRepository)
+    version.set(project.version.toString())
+    type.set(STABLE)
+    modLoaders.add("fabric")
 
-            val builder = GHReleaseBuilder(repository, version as String)
-            builder.name(versionNameText)
-            builder.body(changelogText)
-            builder.commitish(githubBranch)
-            builder.prerelease(rawReleaseType == "beta")
-            builder.create().uploadAsset(file("${project.buildDir}/libs/${base.archivesName.get()}-${version}.jar"), "application/java-archive")
-        }
+    val changelogFile = file("CHANGELOG.md")
+    changelog.set(if (changelogFile.exists()) changelogFile.readText() else "No changelog provided")
+
+    github {
+        accessToken.set(providers.environmentVariable("GITHUB_TOKEN"))
+        repository.set("dvitski/direbats")
+        commitish.set("latest")
     }
-}
 
-env["MODRINTH_TOKEN"]?.run {
     modrinth {
-        token.set(this@run)
-        projectId.set(modrinthId)
-        versionNumber.set(version as String)
-        versionName.set(versionNameText)
-        versionType.set(rawReleaseType)
-        changelog.set(changelogText)
-        uploadFile.set(tasks.getByPath("remapJar"))
-        gameVersions.set(supportedVersions)
-        dependencies {
-            required.project("fabric-api")
-            embedded.project("fabric-language-kotlin")
-        }
+        accessToken.set(providers.environmentVariable("MODRINTH_API_KEY"))
+        projectId.set("epF1CjCx")
+        minecraftVersions.add(versionMinecraft)
     }
-}
 
-env["CURSEFORGE_API_KEY"]?.run {
-    curseforge {
-        apiKey = this@run
-
-        project(closureOf<CurseProject> {
-            id = curseforgeId
-
-            addGameVersion("Fabric")
-            supportedVersions.forEach(::addGameVersion)
-
-            changelog = changelogText
-            changelogType = "markdown"
-            releaseType = rawReleaseType
-
-            mainArtifact(tasks.getByPath("remapJar"), closureOf<CurseArtifact> {
-                displayName = versionNameText
-                relations(closureOf<CurseRelation> {
-                    requiredDependency("fabric-api")
-                    embeddedLibrary("fabric-language-kotlin")
-                })
-            })
-        })
+    curseforge { // god forbid
+        projectId.set("407987")
+        accessToken.set(providers.environmentVariable("CURSEFORGE_TOKEN"))
+        minecraftVersions.add(versionMinecraft)
+        client.set(true)
+        server.set(true)
     }
-}
-
-tasks.register("releaseVersion").configure {
-    dependsOn("build", "modrinth", "github", "curseforge")
 }
